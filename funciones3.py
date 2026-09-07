9482889e44f69a32e3999d8389fd9cdf391a78c0
Michael Alejandro Wong Espinoza <michaelwong@uamv.edu.ni>
2026-09-07 08:17:53 -0600

    Create funciones2

---
+++ funciones3.py
@@ -0,0 +1,10 @@
+def registrar_venta():
+    ventas_registradas = 0
+    ventas_registradas += 1
+    print("Venta registrada")
+
+
+registrar_venta()
+registrar_venta()
+
+print("Total de ventas:", ventas_registradas)
